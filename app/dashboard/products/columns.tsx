2158546252c1cfe626c9bd7d06ab7154b5e8be1e
naufalahmadfauz <naufalahmadfauz@gmail.com>
2025-01-31 15:20:42 +0700

    fix Row import

--- app/dashboard/products/columns.tsx
+++ app/dashboard/products/columns.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import {ColumnDef} from "@tanstack/react-table"
 import Image from "next/image";
 import {
     DropdownMenu,
@@ -15,7 +14,7 @@ import {deleteProduct} from "@/server/actions/delete-product";
 import { toast } from "sonner";
 import Link from "next/link";
 import {VariantsWithImagesTags} from "@/lib/infer-type";
-
+import { ColumnDef, Row } from "@tanstack/react-table"
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
